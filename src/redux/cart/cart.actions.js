58b417d1f8dd6c00ef00041b24b8c7f7c4b47e96
Raf <rafaelmendoza004@gmail.com>
2021-03-19 04:33:41 +0800

    add styling arrow function in checkout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItem = (item) => ({
   type: CartActionsTypes.ADD_ITEM,
   payload: item,
 });
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionsTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
