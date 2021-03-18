55319788f52dd55be80709265f1933e3de4073da
Raf <rafaelmendoza004@gmail.com>
2021-03-19 07:13:36 +0800

    created collection state into redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 import MenuItem from "../menu-item/menu-item.component";
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,41 +1,34 @@
 import React from "react";
 
-import SHOP_DATA from "./shop.data.js";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import shopBanner from "../../assets/yomex-owo-GAN9zxqj5f4-unsplash.jpg";
 import CollectionPreview from "../../components/collection-preview/collection-preview";
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
 import "./shop.styles.scss";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <div className="shopPageContainerTitle">
+      <h1 className="shopPageTitle">SHOP NOW</h1>
+      <p className="shopPageSubtitle">
+        Featuring our latest collections and releases.
+      </p>
+    </div>
+    <div className="shopPageContainer">
+      <div className="overlay"></div>
+      <img className="shopBanner" src={shopBanner} alt="shopBanner" />
+    </div>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        <div className="shopPageContainerTitle">
-          <h1 className="shopPageTitle">SHOP NOW</h1>
-          <p className="shopPageSubtitle">
-            Featuring our latest collections and releases.
-          </p>
-        </div>
-        <div className="shopPageContainer">
-          <div className="overlay"></div>
-          <img className="shopBanner" src={shopBanner} alt="shopBanner" />
-        </div>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
