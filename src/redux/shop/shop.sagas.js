81747d9cde868975329c7d52ebdedff7f494acd5
Raf <rafaelmendoza004@gmail.com>
2021-03-22 11:33:54 +0800

    convert a redux thunk into sagas from promise base approach to sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,27 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
 
+import {
+  firestore,
+  convertCollectionSnapshotToMap,
+} from "../../firebabse/firebase.utils";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
 import ShopActionTypes from "./shop.types";
 
 export function* fetchCollectionsStartAsync() {
   yield console.log("I am Fired");
+
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
